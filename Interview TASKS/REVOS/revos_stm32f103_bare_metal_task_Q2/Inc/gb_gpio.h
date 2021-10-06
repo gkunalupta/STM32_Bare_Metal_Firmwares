caff7dcc887640cf51cee2ebbd750cfa208b0be3
Kunal <kunalguptakew@gmail.com>
2021-10-06 12:23:20 +0530

    USing TIME bASE UNIT F TIMERS to DECODE Digital PULSES

---
+++ Interview TASKS/REVOS/revos_stm32f103_bare_metal_task_Q2/Inc/gb_gpio.h
@@ -0,0 +1,14 @@
+/*
+ * gpio.h
+ *
+ *  Created on: Apr 23, 2021
+ *      Author: kunal
+ */
+
+#ifndef GB_GPIO_H_
+#define GB_GPIO_H_
+
+void Gpio_output_pushpull_conf();
+void Gpio_input_pushpull_conf();
+
+#endif /* GB_GPIO_H_ */
