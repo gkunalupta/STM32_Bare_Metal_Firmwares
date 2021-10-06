9920ad57009b9a5c0bc2788001251bfeeef6cb43
Kunal <kunalguptakew@gmail.com>
2021-10-06 13:22:22 +0530

    USING GPIO and TIMER for generating pulses of different TIME PERIOD

---
+++ Interview TASKS/REVOS/revos_stm32f103_bare_metal_task_Q1/Inc/gb_t_delay.h
@@ -0,0 +1,18 @@
+/*
+ * t_delay.h
+ *
+ *  Created on: Apr 23, 2021
+ *      Author: kunal
+ */
+
+#ifndef GB_T_DELAY_H_
+#define GB_T_DELAY_H_
+
+void timer_initialise();
+void delay_us(uint16_t us);
+void delay_ms(uint16_t ms);
+void delay_sec(uint16_t secs);
+void delay_min(uint16_t min);
+
+
+#endif /* GB_T_DELAY_H_ */
---
+++ Interview TASKS/REVOS/revos_stm32f103_bare_metal_task_Q1/Inc/t_pwm.h
@@ -0,0 +1,15 @@
+/*
+ * t_pwm.h
+ *
+ *  Created on: Apr 23, 2021
+ *      Author: kunal
+ */
+
+#ifndef T_PWM_H_
+#define T_PWM_H_
+
+void T2C1_pin_conf();
+void pwm_init();
+
+
+#endif /* T_PWM_H_ */
