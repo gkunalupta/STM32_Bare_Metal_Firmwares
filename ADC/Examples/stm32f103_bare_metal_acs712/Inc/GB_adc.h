3bb6f6889b1640e2e622767dc941802ddf8616fd
Kunal <kunalguptakew@gmail.com>
2021-10-29 19:48:00 +0530

    acs712 driver for ac and dc current calculation

---
+++ ADC/Examples/stm32f103_bare_metal_acs712/Inc/GB_adc.h
@@ -0,0 +1,34 @@
+/*
+ * GB_adc.h
+ *
+ *  Created on: 18-Sep-2021
+ *      Author: kunal
+ */
+
+#ifndef GB_ADC_H_
+#define GB_ADC_H_
+
+#include "stm32f1xx.h"
+#include <string.h>
+
+/*
+ * ADC Peripheral Initialisation
+ * 1) Clock enable for ADC and ADC Channel PIN Configuration
+ *
+ * 2) ADC Peripheral Configurations
+ * --> Configuration of Trigger Source , Regular Group/Injected Group,
+ * --> Number of ADC Channels to be used
+ * --> enabling and disabling of SCAN,DISCONTINOUS,CONTINOUS modes
+ * --> Confguring ADC Modes, Analog WatchDog
+ *
+ * 3) ADC Channel configuration
+ *   --> Sampling Time of ADC Channel and Sequence of Channels in Regular Groups
+ */
+void adc_init();
+/*
+ * This Function start The ADC Conversion and We wait to get the ADC value by Polling, and then return the ADC Value
+ */
+uint16_t adc_convertPOL_START();
+
+
+#endif /* GB_ADC_H_ */
